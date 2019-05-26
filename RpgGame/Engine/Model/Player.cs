c5bda97202ea7a190ed49b5962feb7d97fe8a6b7
GreatScherzo <barzarin@gmail.com>
2019-05-26 12:33:58 +0900

    Class Creation (Model) and code change (MainWindow.xaml)

---
+++ RpgGame/Engine/Model/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Model
+{
+    class Player
+    {
+        public int Name { get; set; }
+        public string Strength { get; set; }
+        public string Stamina { get; set; }
+        public string Dexterity { get; set; }
+        public string Intelligence { get; set; }
+        public string Level { get; set; }
+        public string Exp { get; set; }
+        public string Gold { get; set; }
+
+        /*
+        int Strength;
+        int Stamina;
+        int Dexterity;
+        int Intelligence;
+
+        int Level;
+        int Exp;
+        int Gold;
+        */
+    }
+}
