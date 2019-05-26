530b30012c712c05d8bd975bd0c601bdf3824ea0
GreatScherzo <barzarin@gmail.com>
2019-05-26 11:16:47 +0900

    starting the repository
    Adding some codes for source control using github. Here we go

---
+++ RpgGame/RpgGame/items/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgGame.items
+{
+    class Weapon
+    {
+        class Sword
+        {
+
+            public string itemDiscription
+            {
+                get { return ("The sword that was meant for the one to bring light to the " +
+                        "to extinguish the misery"); }
+                set { myVar = value; }
+            }
+
+            
+
+
+        }
+    }
+}
